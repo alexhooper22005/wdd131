55ae0f23f404f021206260981f4cb48fa0fad90d
alexhooper22005 <hoo22005@byui.edu>
2025-11-05 12:48:45 -0700

    finished character prove assigment

--- prove/character/character.js
+++ prove/character/character.js
@@ -1,14 +1,49 @@
 const aCharacter = {
     name: 'SnortleBlat',
     class: 'Class: Swamp Beast',
-    level: 'Level: 5',
-    health: 'Health: 100',
-    charlogo: '../images/snortleblat.webp'
+    level: 5,
+    health: 100,
+    charlogo: '../images/snortleblat.webp',
+    attack: function () {
+        if (this.level < 10) {
+            this.health -= 20;
+        }
+        if (this.level >= 10) {
+            this.health -= 10;
+        }
+        if (this.health <= 0) {
+            alert('Character has died');
+            this.health = 100;
+        }
+        
+        return this.health;
+    },
+
+    levelUp: function () {
+        this.level += 1;
+        return this.level;
+    }
 };
+
+
 
 
 document.querySelector('#image').setAttribute('src', aCharacter.charlogo);
 document.querySelector('#name').textContent = aCharacter.name;
 document.querySelector('#class').textContent = aCharacter.class;
-document.querySelector('#level').textContent = aCharacter.level;
-document.querySelector('#health').textContent = aCharacter.health;
+document.querySelector('#level').textContent = `Level: ${aCharacter.level}`;
+document.querySelector('#health').textContent = `Health: ${aCharacter.health}`;
+
+
+
+document.querySelector('#attack').addEventListener('click', function() {
+    aCharacter.attack();
+    document.querySelector('#health').textContent = `Health: ${aCharacter.health}`;
+}); 
+
+document.querySelector('#levelUp').addEventListener('click', function() {
+    aCharacter.levelUp();
+    document.querySelector('#level').textContent = `Level: ${aCharacter.level}`;
+});
+
+
